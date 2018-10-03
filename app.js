de33fcfb935732257fbd542a89bfed5d0eced7cf
Guido Baldessari <Guido.Baldessari@marconi.loc>
2018-10-03 15:33:34 +0200

    package.json and launch.json

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
 console.log('request starting...');
 // respond
-response.write('hello client!');
+response.write('Andreatta chi legge');
 response.end();
 });
 server.listen(3000);
+
+
 console.log('Server running at http://127.0.0.1:3000/');
